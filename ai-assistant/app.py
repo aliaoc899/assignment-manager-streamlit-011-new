5d3e903f8aa27e6ccfa00af2974892e2e5e2cda0
Ali Simaei <aliaocc@wifi-roaming-128-4-178-81.wifi.udel.edu>
2026-05-01 12:25:39 -0400

    day 2 - ai agent design

--- ai-assistant/app.py
+++ ai-assistant/app.py
@@ -7,6 +7,7 @@ import streamlit as st
 
 from dotenv import load_dotenv
 from openai import OpenAI
+
 import json
 from pathlib import Path
 
@@ -24,6 +25,44 @@ if not api_key:
 client = OpenAI(api_key=api_key) #creating an object from the open ai class and
                                 #intializing it with my open ai key
 
+
+#service layer 
+def build_prompt(context_hint: str):
+    return "You are an ai assistant in a online shoping website." \
+    "this is for test. so create some example dataset, use it to response" \
+    "to the user. if the user is asking about the prior conversations, " \
+    "try to reference your answer to the prior conversation"\
+    f"this is the context hint {context_hint}"
+
+def get_ai_response(client:OpenAI, chat_history: list, context_hint:str):
+    # build prompt
+    prompt = build_prompt(context_hint)
+
+    #build prompt message
+    prompt_message = [
+        {
+            'role': 'system',
+            'content': prompt
+        }
+    ]
+
+    # built the final message
+    messages = chat_history + prompt_message
+
+    # call open ai 
+    ai_response = client.chat.completions.create(
+        model="gpt-5-mini",
+        messages=messages,
+        temperature= 1
+    )
+
+    #return the response
+    return ai_response.choices[0].message.content
+
+
+
+
+
 # data layer
 def load_orders(filepath: str):
     json_path = Path(filepath)
@@ -32,7 +71,6 @@ def load_orders(filepath: str):
             return json.load(f)
     else:
         return []
-    
 
 # load logs
 def load_logs(filepath:str):
@@ -49,33 +87,62 @@ def save_logs(filepath: str, logs: list):
     with open(json_path, "w") as f:
         json.dump(logs,f)
 
+orders = load_orders("ai-assistant/orders.json")
+logs = load_logs("ai-assistant/ai_logs.json")
 
 if "messages" not in st.session_state:
     st.session_state['messages'] = []
+    for log in logs:
+        st.session_state['messages'].append(
+            {
+                "role": log['role'],
+                "content": log["content"]
+            }
+        )
 
-
-orders = load_orders("ai-assitant/orders.json")
-logs = load_logs("ai-assitant/ai_logs.json")
-
-for log in logs:
-    st.session_state['messages'].append(
-        {
-            "role": log['role'],
-            "content": log["content"]
-        }
-    )
-
-if len(logs) == 0:
-    st.session_state['messages'].append(
-        {
-             "role": "ai-assistant",
-            "content": "Hi, How can I help you?"
-        }
-    )
+    if len(logs) == 0:
+        st.session_state['messages'].append(
+            {
+                "role": "assistant",
+                "content": "Hi, How can I help you?"
+            }
+        )
 
 
 with st.container(border=True, height=400):
     for message in st.session_state['messages']:
-        with st.container(border=True):
-            with st.chat_message(message['role']):
-                st.markdown(message['content'])
+        with st.chat_message(message['role']):
+            st.markdown(message['content'])
+    
+user_input = st.chat_input("Type your question...")
+
+if user_input:
+    st.session_state['messages'].append(
+        {
+            "role": 'user',
+            'content': user_input
+        }
+    )
+    with st.chat_message('user'):
+        st.markdown(user_input)
+    
+    with st.chat_message('assistant'):
+        with st.spinner("thinking..."):
+            ai_response = get_ai_response(client= client,
+                                          chat_history= st.session_state['messages'],
+                                          context_hint="healthcare")
+            
+            st.markdown(ai_response)
+
+            st.session_state['messages'].append({
+                'role': 'assistant',
+                'content': ai_response
+            })
+    
+    logs = load_logs("ai-assistant/orders.json")
+    logs.append({
+        "user_message": user_input,
+        "assistant_message": ai_response
+    })
+
+    save_logs("ai-assistant/orders.json", logs=logs)
